434c80dfa5dcdd3c4e520bf0fbc2172d68805101
thewatermellonman <davidmaddie10@gmail.com>
2026-09-20 20:30:19 -0400

    subjects

--- script.js
+++ script.js
@@ -1,11 +1,48 @@
-let seconds = 0;
+let seconds = Number(localStorage.getItem("seconds")) || 0;
 let timerInterval = null;
+let studyTime = Number(localStorage.getItem("studyTime")) || 0;
+let subjects = JSON.parse(localStorage.getItem("subjects")) || [];
 
 const timer = document.querySelector(".timer");
 
 const startButton = document.querySelector("#startButton");
 const pauseButton = document.querySelector("#pauseButton");
 const resetButton = document.querySelector("#resetButton");
+
+const subjectSelect = document.querySelector("#subjectSelect");
+const addSubjectButton = document.querySelector("#addSubjectButton");
+
+addSubjectButton.addEventListener("click", function () {
+    const subject = prompt("What are you studying?");
+
+    if (subject === null) {
+        return;
+    }
+
+    if (subject.trim() === "") {
+        alert("Subject cannot be empty.");
+        return;
+    }
+
+    const cleanSubject = subject.trim();
+    if(subjects.includes(cleanSubject)) {
+        alert("Subject already exists.");
+        return;
+    }
+    subjects.push(cleanSubject);
+    localStorage.setItem("subjects", JSON.stringify(subjects));
+    displaySubjects();
+});
+
+function displaySubjects() {
+    subjectSelect.innerHTML = 'option value="">Select a subject</option>';
+    subjects.forEach(function (subject) {
+        const option = document.createElement("option");
+        option.value = subject;
+        option.textContent = subject;
+        subjectSelect.appendChild(option);
+    });
+}
 
 function updateTimer() {
     const hours = Math.floor(seconds / 3600);
@@ -19,15 +56,22 @@ function updateTimer() {
 }
 
 startButton.addEventListener("click", function () {
+    if (subjectSelect.value === "") {
+        alert("choose a subject first");
+        return;
+    }
     if (timerInterval === null) {
         timerInterval = setInterval(function () {
             seconds++;
+            localStorage.setItem("seconds", seconds);
             updateTimer();
         }, 1000);
     }
 });
 
 pauseButton.addEventListener("click", function () {
+    studyTime += seconds;
+    localStorage.setItem("studyTime", studyTime);
     clearInterval(timerInterval);
     timerInterval = null;
 });
@@ -36,5 +80,9 @@ resetButton.addEventListener("click", function () {
     clearInterval(timerInterval);
     timerInterval = null;
     seconds = 0;
+    localStorage.setItem("seconds", seconds);
     updateTimer();
 });
+
+updateTimer();
+displaySubjects();
